82dbb74a8bd908e9aa010ff92503e30b2572f857
Rahul Soni <86958474+Rahul-Soni28@users.noreply.github.com>
2021-08-24 11:48:52 +0530

    Fixed print messages in index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Thisby Rahul Soni\nNode-app'
+  const msg = 'This is demo Node app by Rahul Soni\nNode-app'
   res.end(msg);
 });
 
